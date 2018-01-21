5eb60598afe516869e23b42b77ac8872c9366e47
Frappe <anton.frappe@outlook.com>
2018-01-21 19:12:18 +0100

    BonusUppgift 3

---
+++ D0010E/BonusUppgifter/HelicopterMain.java
@@ -0,0 +1,19 @@
+package BonusUppgifter;
+/** Anton Johansson TCDAA D0010E
+ * 
+ * */
+
+public class HelicopterMain {
+	public static Helicopter h = new Helicopter("Z 41");
+
+	public static void main(String[] args) {
+		h.flyTo(65, 40);
+		h.flyTo(-43, 74);
+		System.out.println(h.callSign() 
+							+ " is at (" + h.getX() + "," 
+							+ h.getY() + ") and has flown " 
+							+ h.distanceFlown() + " km. "
+							+ "The light is turned on: "+ h.lightIsOn()
+							);
+	}
+}
