eabb4a504a90d35de882975edbd6280f8d2a5efe
Anton Johansson <anton.frappe@outlook.com>
2018-02-04 14:53:39 +0100

    Concatinate
    Signed-off-by: Anton Johansson <anton.frappe@outlook.com>

--- D0010E/tutorial/Concatinate.java
+++ D0010E/tutorial/Concatinate.java
@@ -3,7 +3,7 @@ package tutorial;
 public class Concatinate {
 
 	public static void main(String[] args) {
-		if(!(5<7&&5<7))System.out.println("Hej");
+		if(!(5<7&&5>7))System.out.println("Hej");
 	}
 
 }
